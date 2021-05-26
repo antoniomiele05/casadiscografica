9249e8e359ce4fdfa51e8a7e050a76903cbc5d30
Alberto_Mazza <amazza@pxltk.com>
2021-05-26 13:16:30 +0200

    aggiunto persona DTO

---
+++ casaDiscografica/src/main/java/it/example/cd/dto/PersonaDTO.java
@@ -0,0 +1,13 @@
+package it.example.cd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonaDTO {
+	private String nome;
+	private String cognome;
+}
